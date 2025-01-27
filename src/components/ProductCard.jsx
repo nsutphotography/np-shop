260dfcae0414788862a5d68a021a4da00d8a7b4f
Abhishek kumar <abhishekkumar@Abhisheks-MacBook-Air-2.local>
2025-01-27 12:45:54 +0530

    just added the styles to add remove button and the amount to display better

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { Card, CardContent, Typography, Button, CardMedia } from "@mui/material";
+import { Card, CardContent, Typography, Button, CardMedia, Box } from "@mui/material";
 import { CartContext } from "../context/CartContext/CartContext";
 
 const ProductCard = ({ product }) => {
@@ -27,41 +27,48 @@ const ProductCard = ({ product }) => {
                         Product ID: {product._id}
                     </Typography>
                 )}
-                <Typography variant="body2">{product.price}</Typography>
                 <Typography variant="body2">{product.description}</Typography>
+                <div style={{ display: 'flex' }}>
 
 
-
-
-                {/* Add or Remove buttons */}
-                <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-
-                    <Button
-                        variant="contained"
-                        color="primary"
-                        onClick={() => addToCart(product._id)}
-                    // disabled={quantityInCart > 0} // Disable "Add" if the product is already in the cart
+                    <Box
+                        sx={{ display: "flex", alignItems: "center", margin: "0 10px" }}
                     >
-                        Add
-                    </Button>
+                        <Typography variant="body2">{product.price}</Typography>
+                    </Box>
 
 
-                    {quantityInCart > 0 && (
-                        <Typography variant="body2" component="span" sx={{ color: "green", margin: "0.5rem" }}>
-                            {quantityInCart}
-                        </Typography>
-                    )}
-                    {quantityInCart > 0 && (
+
+                    {/* Add or Remove buttons */}
+                    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
 
                         <Button
                             variant="contained"
-                            color="secondary"
-                            onClick={() => removeFromCart(product._id)}
-                        // disabled={quantityInCart === 0} // Disable "Remove" if the product isn't in the cart
+                            color="primary"
+                            onClick={() => addToCart(product._id)}
+                        // disabled={quantityInCart > 0} // Disable "Add" if the product is already in the cart
                         >
-                            Remove
+                            Add
                         </Button>
-                    )}
+
+
+                        {quantityInCart > 0 && (
+                            <Typography variant="body2" component="span" sx={{ color: "green", margin: "0.5rem" }}>
+                                {quantityInCart}
+                            </Typography>
+                        )}
+                        {quantityInCart > 0 && (
+
+                            <Button
+                                variant="contained"
+                                color="secondary"
+                                onClick={() => removeFromCart(product._id)}
+                            // disabled={quantityInCart === 0} // Disable "Remove" if the product isn't in the cart
+                            >
+                                Remove
+                            </Button>
+                        )}
+                    </div>
                 </div>
             </CardContent>
         </Card>
