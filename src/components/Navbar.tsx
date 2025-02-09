6d5c3bb792ab3d7ddbf47991e18f962ba571fd0e
Abhishek kumar <abhishekkumar@Abhisheks-MacBook-Air-2.local>
2025-02-10 02:35:36 +0530

    change navbar for development

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography, Button } from '@mui/material';
+import { AppBar, Toolbar, Typography, Button, Box } from '@mui/material';
 import { Link } from 'react-router-dom';
 import debugLib from 'debug';
 import CartInNavbar from './CartInNavbar';
@@ -18,32 +18,45 @@ const Navbar: React.FC = () => {
           E-Commerce App
         </Typography>
         {import.meta.env.VITE_MODE === "development" && (
-          <>
-            <Button color="inherit" component={Link} to="/order/summary">
-              Order Summary
-            </Button>
-            <Button color="inherit" component={Link} to="/order/history">
-              Order History
-            </Button>
-          </>
+
+            <Box sx={{ flexGrow: 1, display: "flex", justifyContent: "center" }}>
+              <Button component={Link} to="/login">
+                Login
+              </Button>
+              <Button component={Link} to="/signup">
+                Sign Up
+              </Button>
+              <Button component={Link} to="/profile">
+                Profile
+              </Button>
+              <Button color="inherit" component={Link} to="/order/summary">
+                Order Summary
+              </Button>
+              <Button color="inherit" component={Link} to="/order/history">
+                Order History
+              </Button>
+            </Box>
+
+
+
         )}
 
-        <Button color="inherit" component={Link} to="/">
-          Home
-        </Button>
-        <Button color="inherit" component={Link} to="/products">
-          Products
-        </Button>
-        {/* <Button color="inherit" component={Link} to="/checkout">
+      <Button color="inherit" component={Link} to="/">
+        Home
+      </Button>
+      <Button color="inherit" component={Link} to="/products">
+        Products
+      </Button>
+      {/* <Button color="inherit" component={Link} to="/checkout">
           Checkout
         </Button> */}
 
-        {/* Including the Account Menu, Theme Toggle, and Cart */}
-        <AccountMenu />
-        <ThemeToggleButton />
-        <CartInNavbar />
-      </Toolbar>
-    </AppBar>
+      {/* Including the Account Menu, Theme Toggle, and Cart */}
+      <AccountMenu />
+      <ThemeToggleButton />
+      <CartInNavbar />
+    </Toolbar>
+    </AppBar >
   );
 };
 
