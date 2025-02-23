2e624e40e6e928519998fe70b7e448da1e64077a
nsutphotography <nsutphotography@gmail.com>
2025-02-23 14:55:08 +0530

    implement address deletion functionality and update AddressCard component

--- src/context/AddressContext/AddressContext.jsx
+++ src/context/AddressContext/AddressContext.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useState, useEffect, useContext } from 'react';
-import { handleAddAddress, handleFetchAddresses, handleUpdateDefaultAddress } from "./addressAction";
+import { handleAddAddress, handleFetchAddresses, handleUpdateDefaultAddress,handleDeleteAddress } from "./addressAction";
 
 import debugLib from 'debug';
 import { useAuth } from '../AuthContext/AuthContext';
@@ -12,7 +12,7 @@ export const AddressContext = createContext();
 export const AddressProvider = ({ children }) => {
     const [addresses, setAddresses] = useState([]);
     const [error, setError] = useState('');
-const {token}=useAuth();
+    const { token } = useAuth();
     // Fetch addresses on initial load
     useEffect(() => {
         handleFetchAddresses(setAddresses);
@@ -20,25 +20,29 @@ const {token}=useAuth();
 
     // Add an address
     const addAddress = async (newAddress) => {
-        log("add address - in context",newAddress)
+        log("add address - in context", newAddress)
         handleAddAddress(setAddresses, newAddress)
     };
     const updateDefaultAddress = async (addressId) => {
-        log("update default address - in context",addressId)
+        log("update default address - in context", addressId)
         handleUpdateDefaultAddress(setAddresses, addressId)
     };
+    const deleteAddress = async (addressId) => {
+        log("delete address - in context", addressId);
+        handleDeleteAddress(setAddresses, addressId, setError);
+      };
 
 
     return (
-        <AddressContext.Provider value={{ addresses, error, addAddress, updateDefaultAddress }}>
+        <AddressContext.Provider value={{ addresses, error, addAddress, updateDefaultAddress,deleteAddress }}>
             {children}
         </AddressContext.Provider>
     );
 };
 
-export const useAddress = ()=>{
+export const useAddress = () => {
     const context = useContext(AddressContext);
-    if(!context){
+    if (!context) {
         throw new Error("useAddress must be used within an AuthProvider");
     }
     return context;
--- src/pages/CheckoutAddress.tsx
+++ src/pages/CheckoutAddress.tsx
@@ -1,20 +1,22 @@
 import { Typography, Button, Box } from "@mui/material";
 import ShowAddressList from "../components/AddressComponent/ShowAddressList";
 import { Link } from "react-router-dom";
-import debugLib from "debug";
+import { log } from "debug";
 import { useAddress } from "../context/AddressContext/AddressContext";
 
-const log = debugLib("app:address checkout");
+
 
 const CheckoutAddressPage: React.FC = () => {
   const {addresses} = useAddress()
+  // log("address length",addresses[0])
+  // log("address length b",addresses[0]?.addresses?.length)
   return (
     <Box>
       <Box>
         <Typography variant="h4" align="center" gutterBottom>
           Address Information
         </Typography>
-        {addresses.length>0 &&(
+        {addresses[0]?.addresses?.length>0 &&(
 
         <Button variant="contained" color="primary" component={Link} to="/checkout/payment">
           Checkout
